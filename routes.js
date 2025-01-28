c9e66653f32a6e353f5849bc26ff0466753290b2
prashant1879 <prashant1879@gmail.com>
2025-01-28 14:07:24 +0530

    Revert "init"
    This reverts commit bffd61c895b1e858a2e2a640dcedc387fbd81b6f.

--- routes.js
+++
@@ -1,8 +0,0 @@
-'use strict';
-
-let userRoutes = require('./app/routes/user.routes');
-let categoriesRoutes = require('./app/routes/categories.routes');
-module.exports = (app) => {
-  app.use('/api/v1/user', userRoutes);
-  app.use('/api/v1/categories', categoriesRoutes);
-};
